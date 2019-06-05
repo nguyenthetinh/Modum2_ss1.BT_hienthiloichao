e96d14a4ee9e3cd62a18d8caa752161c19caa2aa
nguyenthetinh <tinhnt0207@gmail.com>
2019-06-05 14:31:01 +0700

    hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String a =scanner.nextLine();
+        System.out.println("Hello "+a);
+    }
+}
